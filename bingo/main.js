c2407b06b8aaff390f577278080f288e289d6a25
Enol <enolgp.work@gmail.com>
2024-06-14 22:33:19 +0200

    BINGO - Added function to read data from array

--- bingo/main.js
+++ bingo/main.js
@@ -1,48 +1,53 @@
-// Change a color into the same but grayer
-// Cambiar a escala de grises
-function rgbToGrayscale(r, g, b) {
-    r = Math.max(r - 120, 0); // Reducir rojo
-    g = Math.max(g - 120, 0); // Reducir verde
-    b = Math.max(b - 120, 0); // Reducir azul
 
-    // Construir el nuevo color en formato RGB
+// Function to shuffle array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Change a color into the same but darker
+function rgbToGrayscale(r, g, b) {
+    r = Math.max(r - 120, 0);
+    g = Math.max(g - 120, 0);
+    b = Math.max(b - 120, 0);
+
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-// Función para aplicar flip y cambio de color
+// Flip the card and change color
 function applyFlipAndGray(cell) {
-    if (!cell.classList.contains('flipped')) { // Asegura que solo se ejecute una vez
-        cell.classList.add('flipped'); // Marca la celda como flipped
+    if (!cell.classList.contains('flipped')) { 
+        cell.classList.add('flipped'); 
 
-        // Aplicar flip-forward
         cell.classList.add('flip-forward');
 
         setTimeout(() => {
-            // Cambiar el color de fondo a escala de grises
             const style = window.getComputedStyle(cell);
             const bgColor = style.backgroundColor;
             const [r, g, b] = bgColor.match(/\d+/g).map(Number);
             cell.style.backgroundColor = rgbToGrayscale(r, g, b);
             cell.classList.add('text-white');
 
-            // Aplicar flip-backward después de cambiar el color
             setTimeout(() => {
                 cell.classList.remove('flip-forward');
                 cell.classList.add('flip-backward');
-            }, 10); // Añadir un pequeño retardo para aplicar flip-backward después de cambiar el fondo
-        }, 300); // Tiempo igual a la duración de la animación forward
+            }, 10); 
+        }, 300); 
     }
     cell.classList.remove("cell");
 }
 
-// Create a new cell with 
+// Create a new cell with the parameters
 function createCell(id, content) {
     const newCell = document.createElement('div');
     newCell.classList.add('col-sm-4', 'border', 'text-center', 'p-5', 'cell');
     newCell.id = id;
     newCell.textContent = content;
 
-    // Agregar evento click
+
     newCell.addEventListener('click', () => {
         applyFlipAndGray(newCell);
     });
@@ -53,8 +58,10 @@ function createCell(id, content) {
 // Stablish the Bingo sheet
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('cell-container');
-    for (let i=1; i<=9 ;i++){
-        let cell = createCell(i,i)
+    maxLength = data.length
+    let shuffledData=shuffleArray(data.slice())
+    for (let i=0; i<=maxLength ;i++){
+        let cell = createCell(i,data[i])
         if(i%2)
             cell.classList.add('c1')
         else
